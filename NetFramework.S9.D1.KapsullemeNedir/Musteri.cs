e2f4219ea51e3f70836514998cc80814713b51f4
akifkiraz <69064758+akifk11@users.noreply.github.com>
2023-07-27 21:14:28 +0300

    kapsulleme

---
+++ NetFramework.S9.D1.KapsullemeNedir/Musteri.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S9.D1.KapsullemeNedir
+{
+    public class Musteri
+    {
+
+        public Musteri() {
+            this.id = IDUret();
+        }
+        // Class => Field
+        private int id;
+        public int ID { get 
+            {
+                return this.id;
+            }
+            private set
+            {
+                this.id = value;
+            }
+        }
+        public string isim;
+        private string _soyisim;
+        public string Soyisim
+        {
+            get { return this._soyisim; }
+            set { this._soyisim = value;
+                this.emailAdres = string.Format("{0}.{1}@hotmail.com",isim,_soyisim);
+            }
+        }
+
+
+
+
+        // Class => property
+        private string emailAdres;
+        public string EmailAdres
+        {
+            // get; // database veya farklı bir veri kaynağı ..class çağıran programcıya datayı gösterdiğimiz kısım
+            // set; // dış dünyadan alınan datanın içeride private olarak saklanan Field içerisine atadığımız kısım
+
+            private set
+            {
+               this.emailAdres = value; 
+            }
+            get
+            {
+                return this.emailAdres;
+            }
+        }
+
+        // Ara ödev 
+        private string _tckimliknumarasi;
+        public string TCKimlikNumarasi
+        {
+            set
+            {
+                if (value.Length == 11)
+                {
+                    bool bayrak = false;
+                    for(int i = 0; i < value.Length; i++)
+                    {
+                        bool karakterKontrol = char.IsNumber(value[i]);
+                        if (karakterKontrol)
+                        {
+                            // sayısal değer demektir
+                        }
+                        else
+                        {
+                            bayrak = true;
+                            break;
+                        }
+
+                        
+                    }
+                    if (bayrak)
+                    {
+                        Console.WriteLine("TC kimlik numarası içerisindeki değerler sayısal olmalıdır");
+                    }
+                    else { this._tckimliknumarasi = value; }
+                }
+            }
+            get {
+                return this._tckimliknumarasi.Substring(0,3); 
+            }
+        }
+
+        private int IDUret()
+        {
+            Random Rnd = new Random();
+            return Rnd.Next(10000,90000);
+        }
+    }
+}
---
+++ NetFramework.S9.D1.KapsullemeNedir/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S9.D1.KapsullemeNedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1 = new Musteri();
+            M1.isim = "akif";
+            // M1.soyisim = "kiraz";
+
+            /*
+             * Field değer ataması yapılamasın ama değer okunabilsin
+             * Field içersine değer ataması yapılsın ama değer okunamasın
+             * Field değer atamasının ama ilk 4 hanesi okunsun
+             * Field okuma ve yazma yapılsın ama bir süreçten geçsin sonra gösterilsin
+             */
+
+            // Class
+            // Field
+            // Property
+            // Get & Set
+
+            // M1.emailAdres = "akif@hotmail.com"; (Field)
+
+            // Genel kapsülleme
+            // M1.EmailAdres = "akif@hotmail.com";
+            Console.Write(M1.EmailAdres);
+
+            // 1.Field değer ataması yapılamasın ama değer okunabilsin
+            Console.WriteLine("Musteri Id Değeri" + M1.ID.ToString());
+            // M1.ID = 10;
+
+            M1.TCKimlikNumarasi = "78945612399";
+
+            string TC = M1.TCKimlikNumarasi;
+            Console.WriteLine("TC kimlik numarası:{0}", TC);
+
+            // isim soyisim 
+            // email adresi
+            M1.isim = "akif";
+            M1.Soyisim = "kiraz";
+
+            Console.WriteLine(M1.EmailAdres);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
